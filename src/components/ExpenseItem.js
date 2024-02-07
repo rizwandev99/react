bf71c3cb1d0b91f3603599e78eacd09e5d62be4c
rizwandev99 <rizwandev99@gmail.com>
2024-02-07 15:55:26 +0530

    added Expense Item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense items:</h2>
+      <p>Food Rs10</p>
+      <p>Petrol Rs100</p>
+      <p>Movies Rs100</p>
+    </div>
+  );
+}
+export default ExpenseItem;
